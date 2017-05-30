9fb92271ccae6e476d775928fb75358bf66987dd
Christopher Hight <Chris@christophers-macbook-pro.local>
2017-05-29 22:29:32 -0700

    Initial Commit

---
+++ TTR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TTR
+//
+//  Created by Christopher Hight on 5/29/17.
+//  Copyright © 2017 Chris Hight. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
